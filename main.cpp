ec77502c1c4558dc438fcc7f5b7da10d824ab4ed
Max Jesus <maxjeseuss@gmail.com>
2020-10-27 22:45:11 -0700

    Created menu in main.

--- main.cpp
+++ main.cpp
@@ -3,9 +3,33 @@
 
 
 int main() {
+
     dictionary ten;
-    ten.printWords();
-//    ten.addWord();
-    std::cout << "Hello, World!" << std::endl;
+    int choice;
+    bool endCon = false;
+    while(!endCon) {
+        cout << "1 - Print dictionary" << endl <<
+             "2 - Find word definition" << endl <<
+             "3 - Enter new word and definition" << endl <<
+             "4 - Quit" << endl;
+        cin >> choice;
+        switch (choice) {
+
+            case 1:
+                ten.printWords();
+                break;
+            case 2:
+                ten.getDefinition();
+                break;
+            case 3:
+                ten.addWord();
+                break;
+            case 4:
+                endCon = true;
+                break;
+        }
+    }
+
+    std::cout << "Quit" << std::endl;
     return 0;
 }
