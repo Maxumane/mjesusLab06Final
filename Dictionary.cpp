56670acdbb540a20347762f44f56aa7ae9ff33cf
Max Jesus <maxjeseuss@gmail.com>
2020-10-27 22:41:52 -0700

    Added add word function.

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -52,3 +52,34 @@ void dictionary::getDefinition() {
         cout << oneNumber->first << " - " << oneNumber->second << endl;
     }
 }
+
+
+void dictionary::addWord() {
+    string addKey;
+    string definition;
+    cout << "Enter the word you'd like to add" << endl;
+    cin >> addKey;
+    std::for_each(addKey.begin(), addKey.end(), [](char & c){
+        c = ::tolower(c);
+    });
+    map<string, string>::iterator oneNumber = wordMap.find(addKey);
+    while(oneNumber != wordMap.end()) {
+        cout << "That world already exists in the dictionary, enter a new one." << endl;
+        cin >> addKey;
+        oneNumber = wordMap.find(addKey);
+    }
+
+    if (oneNumber == wordMap.end()) {
+        cout << "Add definition below " << endl;
+        cin.ignore();
+        getline(cin, definition);
+        wordMap.insert(make_pair(addKey, definition));
+        odictDoc.open("dictionary.txt", std::ios_base::app);
+
+
+        odictDoc << addKey << endl << definition << endl;
+        odictDoc.close();
+    }
+    cout << "Added" << endl;
+}
+
