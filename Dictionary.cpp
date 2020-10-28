eeacde052333cf5aa69239a4f7c9f3faa82c1df7
Max Jesus <maxjeseuss@gmail.com>
2020-10-27 22:41:06 -0700

    Added destructor, definition getter, and dictionary printout

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -19,3 +19,36 @@ dictionary::dictionary() {
         wordMap.insert(make_pair(word, definition));
     }
 }
+
+
+dictionary::~dictionary() {
+    odictDoc.close();
+    idictDoc.close();
+}
+
+void dictionary::printWords() {
+
+    for(auto it = wordMap.begin(); it != wordMap.end(); ++it)
+    {
+        cout << it->first << " - " << it->second << endl;
+    }
+
+}
+
+void dictionary::getDefinition() {
+    string searchKey;
+    cout << "Enter the word you're looking for." << endl;
+    cin >> searchKey;
+    std::for_each(searchKey.begin(), searchKey.end(), [](char & c){
+        c = ::tolower(c);
+    });
+    map<string, string>::iterator oneNumber = wordMap.find(searchKey);
+
+
+
+    if (oneNumber == wordMap.end()) {
+        cout << "The word doesn't exist in the dictionary" << endl;
+    } else {
+        cout << oneNumber->first << " - " << oneNumber->second << endl;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "Dictionary.hpp"
+
 
 int main() {
+    dictionary ten;
+    ten.printWords();
+//    ten.addWord();
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
